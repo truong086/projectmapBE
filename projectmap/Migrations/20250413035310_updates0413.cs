9a3ae73a211c434f56af0277b1414d33b68eae54
truong086 <91019609+truong086@users.noreply.github.com>
2025-04-14 09:30:21 +0800

    update0414

---
+++ projectmap/Migrations/20250413035310_updates0413.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace projectmap.Migrations
+{
+    public partial class updates0413 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
